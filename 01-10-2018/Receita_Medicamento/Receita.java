c38cd2eb8b3fdefcf1be99425b618324d91f7a10
Dimitri Marinho <dimitri.santana.marinho@gmail.com>
2018-10-03 13:52:02 -0300

    Delete unnecessary things

--- 01-10-2018/Receita_Medicamento/Receita.java
+++ 01-10-2018/Receita_Medicamento/Receita.java
@@ -1,59 +1,37 @@
 
-/**
- * Write a description of class Receita here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
-public class Receita
-{
-    // instance variables - replace the example below with your own
+public class Receita{
     private String paciente;
     private Medicamento medicamentos[];
     private int tam;
-
-    /**
-     * Constructor for objects of class Receita
-     */
-    public Receita(String paciente, int quantidade)
-    {
+    public Receita(String paciente, int quantidade){
         this.paciente = paciente;
         this.medicamentos = new Medicamento[quantidade];
         this.tam = 0;
         adicionarTodos();
     }
-
-    public Receita(String paciente)
-    {
+    public Receita(String paciente)    {
         this(paciente, 1);
-    }
-    
-    public boolean addRemedio(String nome, String principioAtivo, String indicacao)
-    {
+    }    
+    public boolean addRemedio(String nome, String principioAtivo, String indicacao){
         boolean adicionado = true;
-        if(tam < this.medicamentos.length)
-        {
+        if(tam < this.medicamentos.length){
             this.medicamentos[tam] = new Medicamento(nome, principioAtivo, indicacao);
             tam++;
         }
-        else
-        {
+        else{
             adicionado = false;
         }
         return adicionado;
-    }
-    
-    public boolean adicionarTodos()
-    {
+    }    
+    public boolean adicionarTodos(){
         boolean adicionados = true;
         String nome="teste", principioAtivo="teste", indicacao="teste";
-        if(tam<medicamentos.length)
-        {
-            for (int i=tam; i<medicamentos.length; i++)
-            {
+        if(tam<medicamentos.length){
+            for (int i=tam; i<medicamentos.length; i++){
                 this.medicamentos[i] = new Medicamento(nome, principioAtivo, indicacao);   
             }
-        }   else
+        }
+        else
             adicionados = false;
         return adicionados;
     }
