425b93af73a6915bab01262df3036256fce5e862
Dimitri Marinho <dimitri.santana.marinho@gmail.com>
2018-10-03 13:50:40 -0300

    Delete unnecessary things

--- 01-10-2018/Receita_Medicamento/Medicamento.java
+++ 01-10-2018/Receita_Medicamento/Medicamento.java
@@ -1,37 +1,18 @@
-
-/**
- * Write a description of class Medicamento here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
 public class Medicamento
 {
-    // instance variables - replace the example below with your own
     private String nome, principioAtivo, indicacao;
-
-    /**
-     * Constructor for objects of class Medicamento
-     */
-    public Medicamento(String nome, String principioAtivo, String indicacao)
-    {
+    public Medicamento(String nome, String principioAtivo, String indicacao){
         this.nome = nome;
         this.principioAtivo = principioAtivo;
         this.indicacao = indicacao;
-    }
-    
-    public String getNome()
-    {
+    }    
+    public String getNome(){
         return this.nome;
-    }
-    
-    public String getPrincipioAtivo()
-    {
+    }    
+    public String getPrincipioAtivo(){
         return this.principioAtivo;
-    }
-    
-    public String getIndicacao()
-    {
+    }    
+    public String getIndicacao(){
         return this.indicacao;
     }
 }
