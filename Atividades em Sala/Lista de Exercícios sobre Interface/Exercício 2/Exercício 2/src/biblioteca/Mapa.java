4501f820888ed088fdd75a6db190aa4920ac388f
Dimitri Marinho <dimitri.santana.marinho@gmail.com>
2018-11-28 11:43:40 -0300

    Doing Exercise 2 ...

---
+++ Atividades em Sala/Lista de Exercícios sobre Interface/Exercício 2/Exercício 2/src/biblioteca/Mapa.java
@@ -0,0 +1,42 @@
+package biblioteca;
+
+public class Mapa {
+
+		private String titulo, descricaoSumaria;
+		private double escala;
+		
+		public Mapa(String titulo, String descricaoSumaria, double escala) {
+			this.titulo = titulo;
+			this.descricaoSumaria = descricaoSumaria;
+			this.escala = escala;
+		}
+
+		public String getTitulo() {
+			return titulo;
+		}
+
+		public void setTitulo(String titulo) {
+			this.titulo = titulo;
+		}
+
+		public String getDescricaoSumaria() {
+			return descricaoSumaria;
+		}
+
+		public void setDescricaoSumaria(String descricaoSumaria) {
+			this.descricaoSumaria = descricaoSumaria;
+		}
+
+		public double getEscala() {
+			return escala;
+		}
+
+		public void setEscala(double escala) {
+			this.escala = escala;
+		}
+		@Override
+		public String toString() {
+			return "Mapa [titulo=" + titulo + ", descricaoSumaria=" + descricaoSumaria + ", escala=" + escala + "]";
+		}
+		
+}
